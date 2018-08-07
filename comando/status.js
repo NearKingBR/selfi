621acf3bf46522e0fa688b580d6889f55eb5e329
Near <37019421+NearKingBR@users.noreply.github.com>
2018-08-07 00:44:43 -0300

    Update status.js

--- comando/status.js
+++ comando/status.js
@@ -1,4 +1,17 @@
-exports.run = (client, message, args) => {
-    message.channel.send(`Em funcionamento!!!! total de ${client.channels.size} Canais, em ${client.guilds.size} servers, um total de ${client.users.size} usuarios.`)
+    exports.run = (client, message, args) => {
 
-};
+        let sincon = message.guild.iconURL;
+        let statusEmbed = new Discord.RichEmbed()
+        .setTitle("Estou em Funcionamento, Senhor!!")
+        .addBlankField()
+        .setColor("#aaff00")
+        .setDescription("Informações sobre min:")
+        .addField("Minha quantidade atual de Canais é:", `${client.channels.size}`)
+        .addField("Minha Quantidade atual de Servidores é:", `${client.guilds.size}`)
+        .addField("Minha quantidade atual de Usuários é:", `${client.users.size}`)
+        .setThumbnail(sincon)
+        .setTimestamp();
+    
+
+        return message.channel.send(statusEmbed);
+    };
