e5b1fac93c246ece309532274851b180b487d6d2
Near <37019421+NearKingBR@users.noreply.github.com>
2018-10-06 13:56:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const config = require('./config.json');
 
 
 client.on('guildMemberAdd', member =>{
-  member.send('Oiee nenem, ta no tédio? Entra no Discord Crazy-Life, Super recomendo, La muita coisa legal, Eventos Diarios, Desafios, e muitos Chats para Interação, Amizades etc.. :smiling_imp::smiling_imp::heart_eyes::heart:️ https://discord.gg/DSs3u38 ')
+  member.send('Oiee nenem, ta no tédio? Entra no Discord NightBoard, Super recomendo, La tem muita coisa legal, Eventos Diarios, Desafios, e muitos Chats para Interação,Uma Comunidade Gamer Bem animada, Amizades etc.. :smiling_imp::smiling_imp::heart_eyes::heart:️ https://discord.gg/9cwstfA ')
 });
 
 client.on("message", message => {
