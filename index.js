07a35dbe186e9771e0a3f3b5fbbde6bd5a10b522
Near <37019421+NearKingBR@users.noreply.github.com>
2018-11-29 17:08:48 -0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const config = require('./config.json');
 
 
 client.on("ready", async () => {
-    client.user.setGame('https://discord.me/NightBoard', 'https://www.twitch.tv/Near');
+    client.user.setActivity('https://discord.me/NightBoard', 'https://www.twitch.tv/Near');
 });
 
 client.on('guildMemberAdd', member =>{
